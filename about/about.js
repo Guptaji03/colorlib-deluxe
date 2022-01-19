c34576fd2a501d46af847006fc2fc78bdc22f0a1
Shubham168 <jadhavshubham168@gmail.com>
2022-01-19 12:54:43 +0530

    added the counter to display numbers from 0 to target

--- about/about.js
+++ about/about.js
@@ -1,4 +1,33 @@
+// start of counter
 
+const counters = document.querySelectorAll(".counter");
+
+const speed = 200;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const inc = target / 100;
+    console.log(count,target);
+    if (count < target) {
+      counter.innerText = count+inc;
+      setTimeout(updateCount,20);
+      
+    }
+    else{
+      count.innerText = target;
+    }
+  }
+  updateCount(); // start the timeout
+});
+
+// end of counter
+
+
+
+//Start of animation
 const cards = document.querySelectorAll(".animate");
 
 const options = {
@@ -10,10 +39,13 @@ const socialObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
       entry.target.style.animation = "transitionIn 0.75s ease-out";
-    } 
+    }
   });
 }, options);
 
 cards.forEach((card) => {
   socialObserver.observe(card);
 });
+//end of animation
+
+
