34affd049c7a07ed83045b2b11988e5ae632a50b
Shubham168 <jadhavshubham168@gmail.com>
2022-01-22 22:05:34 +0530

    finalized about,contact,blog,most_of_home

---
+++ home/footer.js
@@ -0,0 +1,101 @@
+document.write(
+    `
+    <footer>
+    <div class="main">
+        <div class="col col1">
+            <h3>Deluxe Hotel</h3>
+
+            <div class="content">
+                <div class="content-inner1">
+                    Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+                </div>
+            </div>
+
+            <div class="media animate">
+                <div class="media-inner">
+                    <i class="fa fa-twitter circle"></i>
+    
+                </div>
+                <div class="media-inner">
+                    <i class="fa fa-facebook circle"></i>
+    
+                </div>
+                <div class="media-inner">
+                    <i class="fab fa-instagram"></i>
+    
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col col2">
+            <h3>Useful Links</h3>
+
+            <div class="content">
+                <div class="content-inner" onclick="redirect()"><span>Blog</span></div>
+                <div class="content-inner" onclick="redirect()"><span>Rooms</span></div>
+                <div class="content-inner" onclick="redirect()"><span>Amenities</span></div>
+                <div class="content-inner" onclick="redirect()"><span>Gift Card</span></div>
+            </div>
+
+
+        </div>
+
+
+        <div class="col col3">
+            <h3>Privacy</h3>
+
+            <div class="content">
+                <div class="content-inner" onclick="redirect()"><span>Career</span></div>
+                <div class="content-inner" onclick="redirect()"><span>About Us</span></div>
+                <div class="content-inner" onclick="redirect()"><span>Contact Us</span></div>
+                <div class="content-inner" onclick="redirect()"><span>Services</span></div>
+            </div>
+
+
+
+        </div>
+
+
+        <div class="col col4">
+
+            <h3>Have a Questions?</h3>
+
+            <div class="content">
+                <div class="content-inner1">
+                    <div class="icon">
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                    </div>
+                    <div class="content-inner-inner add">
+                        203 Fake St. Mountain View, San Francisco, California, USA
+                    </div>
+                </div>
+                <div class="content-inner1">
+                    <div class="icon">
+                        <i class="fas fa-phone-alt"></i>
+                    </div>
+                    <div class="content-inner-inner cont">
+                        +2 392 3929 210
+                    
+                    </div>
+                </div>
+                <br>
+                <div class="content-inner1">
+                    <div class="icon">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                    </div>
+                    <div class="content-inner-inner ema">
+                        info@yourdomain.com
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bottom">
+        Copyright ©2022 All rights reserved | This template is made with <i class="fas fa-heart"></i>
+        by <a href="">Colorlib</a> 
+    </div>
+</footer>
+    `
+)
