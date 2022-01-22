b47bf5ffa1260aa3ce5ab88e1a2fea7917d30594
gayathripriyaG <gayamgayathripriya@gmail.com>
2022-01-22 22:14:31 +0530

    upadated footer,insta and nav

---
+++ rooms/rooms-insta/instagram.js
@@ -0,0 +1,41 @@
+document.write(
+    `
+    <!--------------- instagran part--------- -->
+    <div class="main1">
+        <div class="instagram">
+            <h1>Instagram</h1>
+              <hr id="big">
+              <hr id="small">
+        </div>
+        <div class="images animate">
+            
+            <div class="img1 animate"> 
+                <!-- <img src="https://preview.colorlib.com/theme/deluxe/images/insta-1.jpg" alt="image> -->
+               <div class="insta1 icons">
+                     <i class="fab fa-instagram"></i></div>
+            </div>
+            <div class="img2 animate"> 
+                <div class="insta2 icons">
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+            <div  class="img3 animate">
+                <div class="insta3 icons">
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+            <div class="img4 animate">
+                <div class="insta4 icons">
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+            <div class="img5 animate">
+                <div class="insta5 icons">
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="footer.js"></script>`
+)
