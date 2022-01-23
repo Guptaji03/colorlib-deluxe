77e3e326f52117367312cc2eb9a64b9bc2de7b15
Shubham168 <jadhavshubham168@gmail.com>
2022-01-23 20:45:32 +0530

    final submission

--- home/nav.js
+++ home/nav.js
@@ -3,27 +3,27 @@ document.write(
     <header class = "m-navheader">
       <div class="mnav-container">
           <nav class="m-nav">
-              <a href="index.html" class="m-logo">DELUXE</a>
+              <a href="./home.html" class="m-logo">DELUXE</a>
               <input type = "checkbox" id = "m-check" class="m-checkbtn">
               <label for="m-check" class="m-checkbtn">&nbsp;<i class = "fas fa-bars"><span class ="m-menutext">&nbsp;MENU&nbsp;</span></i></label>
               <ul class="m-nav-list">
                   <li>
-                      <a href="index.html" class="m-nav-link homeon">Home</a>
+                      <a href="./home.html" class="m-nav-link homeon">Home</a>
                   </li>
                   <li>
-                      <a href="rooms.html" class="m-nav-link">Rooms</a>
+                      <a href="../rooms/rooms.html" class="m-nav-link">Rooms</a>
                   </li>
                   <li>
-                      <a href="#" class="m-nav-link">Restaurant</a>
+                      <a href="../restaurant/restaurant.html" class="m-nav-link">Restaurant</a>
                   </li>
                   <li>
-                      <a href="#" class="m-nav-link">About</a>
+                      <a href="../about/about.html" class="m-nav-link">About</a>
                   </li>
                   <li>
-                    <a href="#" class="m-nav-link">Blog</a>
+                    <a href="../fullblog/blog.html" class="m-nav-link">Blog</a>
                 </li>
                   <li>
-                      <a href="#" class="m-nav-link">Contact</a>
+                      <a href="../fullcontact/contact.html" class="m-nav-link">Contact</a>
                   </li>
               </ul>
               
